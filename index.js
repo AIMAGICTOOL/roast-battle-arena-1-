48621e584d3b407058e17fdbc92769bc8a77cfba
AIMAGICTOOL <shivanshuarya95@gmail.com>
2025-05-20 14:08:13 +0530

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(path.join(__dirname, '../frontend')));
+
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
