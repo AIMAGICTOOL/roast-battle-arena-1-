50039d3f771f7be9d84160b11c8e75677c144d86
AIMAGICTOOL <shivanshuarya95@gmail.com>
2025-06-15 22:07:20 +0530

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,19 +1,21 @@
 // public/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getDatabase, ref, push, onChildAdded } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
-import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCOZZStypUDTr6shxcB0kM6ddJ9HaNyM2Q",
   authDomain: "roast-battle-arena-6658b.firebaseapp.com",
-  databaseURL: "https://roast-battle-arena-6658b-default-rtdb.firebaseio.com",
+  databaseURL: "https://roast-battle-arena-6658b-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "roast-battle-arena-6658b",
-  storageBucket: "roast-battle-arena-6658b.appspot.com",
+  storageBucket: "roast-battle-arena-6658b.firebasestorage.app",
   messagingSenderId: "1004753319883",
   appId: "1:1004753319883:web:ddcd82fa55d2db6ec8e84e"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const db = getDatabase(app);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
